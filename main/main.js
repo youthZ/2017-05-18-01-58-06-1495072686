ba7e86e346703be4666ae090e66c5ae9935690d6
youths <552099787@qq.com>
2017-05-23 11:25:42 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
-module.exports = function main() {
+module.exports = function main(str) {
   // Write your cade here
+  var sum = 0;
+  for (var i = 0; i < str.length ; i++)
+  {
+	sum+=parseInt(str.substring(i,i+1));
+  }
+  return sum;
 };
